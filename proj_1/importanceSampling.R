6d70466a58df7eea4bf5ebb871d39e3b0ad38de1
Thyholt <a.thyholt.henriksen@outlook.com>
2018-02-06 12:01:47 +0100

    allmost working importance sampling

--- proj_1/importanceSampling.R
+++ proj_1/importanceSampling.R
@@ -7,7 +7,7 @@ computeWeights <- function(alpha_new, alpha_prop, x, q) {
 	k <- length(q)
 	# Using log scale
 	weights <- lgamma(sum(alpha_prop + x)) - lgamma(sum(alpha_new + x)) +
-	       	   sum(lgamma(alpha_new + x) - lgamma(alpha_prop + z)) + 
+	       	   sum(lgamma(alpha_new + x) - lgamma(alpha_prop + x)) + 
 		   sum(((alpha_new - alpha_prop) * q)[-K]) +
 		   (alpha_new[K] - alpha_prop[K]) * log(1 - sum(q[-K])) 
 	# Return in regular scale
@@ -21,7 +21,7 @@ importanceSampling <- function(prior, m, alpha_new, alpha_prop) {
 
 	# Calculate previous q_i
 	q_prop <- sampleDirichlet(alpha_prop, 4) 	
-
+  print(q_prop)
 	# Compute weighted average
 	weights <- computeWeights(alpha_new, alpha_prop, student_distr, q_prop)
 	
@@ -29,3 +29,14 @@ importanceSampling <- function(prior, m, alpha_new, alpha_prop) {
 }
 
 # Test functionality 
+alpha_prop <- matrix(c(0.5, 0.5, 0.5, 0.5))
+alpha_new <- matrix(c(1, 1, 1, 1))
+
+students <- 35
+draws <- 100
+
+daysInSeason <- matrix(c(92, 92, 91, 90))
+
+prior <- matrix(c(92/365, 92/365, 91/365, 90/365))
+
+print(importanceSampling(prior, students, alpha_new, alpha_prop))
