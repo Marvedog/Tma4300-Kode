7b28787d5ae3bcd27ed795f721cf02172c811456
Marvedog <marcusae@stud.ntnu.no>
2018-02-06 21:25:50 +0100

    add code

--- proj_1/exercise1samplePosteriorMean.R
+++ proj_1/exercise1samplePosteriorMean.R
@@ -5,40 +5,9 @@ source("sampleP.R")
 students <- 35
 draws <- 100000
 
-spring <- 92
-summer <- 92 
-autumn <- 91
-winter <- 90
-
-daysInSeason <- matrix(1, 4, 1)
-daysInSeason[1] <- spring
-daysInSeason[2] <- summer
-daysInSeason[3] <- autumn
-daysInSeason[4] <- winter
-
-daysYear <- 365
-
-p1 <- 92 / 365
-p2 <- 92 / 365
-p3 <- 91 / 365
-p4 <- 90 / 365
-
-p <- matrix(1, 4, 1)
-p[1] <- p1
-p[2] <- p2
-p[3] <- p3
-p[4] <- p4
+daysInSeason <- matrix(c(92, 92, 91, 90))
+p <- matrix(c(92 / 365, 92 / 365, 91 / 365, 92 / 365))
 
 means <- computePosteriorMeans(p, students, draws, daysInSeason)
 means_sorted <- sort(means)
 truehist(means_sorted[2500:97500])
-print(mean(means))
-
-N <- matrix(1, 4, 1)
-
-N[1] <- 9
-N[2] <- 9
-N[3] <- 9
-N[4] <- 8
-
-print(calcP(p, N,daysInSeason))
